abde6847b20da6da7b20680360ccd448578c9c82
mansisindhu <mansisindhu2909@gmail.com>
2022-03-05 16:21:02 +0530

    Add filter by region

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,17 +1,27 @@
 import { useState } from "react";
 import { AiOutlineSearch } from "react-icons/ai";
+import { MdKeyboardArrowDown } from "react-icons/md";
 
 import Navbar from "../components/Navbar";
 
+const filterOptions = ["Africa", "America", "Asia", "Europe", "Oceania"];
+
 const HomePage = () => {
   const [search, setSearch] = useState("");
+  const [currentFilter, setCurrentFilter] = useState("Filter by Region");
+  const [filterOpen, setFilterOpen] = useState(false);
+
+  const handleFiltersText = (text) => {
+    setCurrentFilter(text);
+    setFilterOpen(false);
+  };
 
   return (
     <>
       <div className="app-container">
         <main className="main">
           <Navbar />
-          <div className="filters">
+          <div className="sub-navbar">
             <div className="search">
               <AiOutlineSearch className="icon" />
               <input
@@ -21,7 +31,27 @@ const HomePage = () => {
                 placeholder="Search for a country..."
               />
             </div>
-            <div className="filters"></div>
+            <div className="filters">
+              <div
+                className="filter-heading"
+                onClick={() => setFilterOpen((prev) => !prev)}
+              >
+                <p>{currentFilter}</p>
+                <MdKeyboardArrowDown />
+              </div>
+              {filterOpen ? (
+                <div className="options">
+                  {filterOptions.map((el) => (
+                    <div
+                      onClick={() => handleFiltersText(el)}
+                      className="option"
+                    >
+                      {el}
+                    </div>
+                  ))}
+                </div>
+              ) : null}
+            </div>
           </div>
         </main>
       </div>
@@ -31,6 +61,13 @@ const HomePage = () => {
             background-color: #fafafa;
             padding: 12px;
             margin-top: 64px;
+            height: 100vh;
+          }
+
+          .sub-navbar {
+            display: flex;
+            flex-direction: column;
+            gap: 32px;
           }
 
           .search {
@@ -60,6 +97,45 @@ const HomePage = () => {
           .search :global(.icon) {
             color: hsl(0, 1%, 72%);
             font-weight: 500;
+          }
+
+          .filters {
+            max-width: 200px;
+            position: relative;
+          }
+
+          .filter-heading {
+            display: flex;
+            align-items: center;
+            padding: 18px;
+            background-color: #fff;
+            border-radius: 4px;
+            box-shadow: 0 1px 5px rgb(0 0 0 / 20%);
+            gap: 12px;
+            justify-content: space-between;
+            cursor: pointer;
+          }
+
+          .filter-heading > p {
+            font-size: 12px;
+          }
+
+          .options {
+            position: absolute;
+            margin-top: 2px;
+            background-color: #fff;
+            width: 200px;
+            border-radius: 4px;
+            display: flex;
+            flex-direction: column;
+            gap: 4px;
+            font-size: 12px;
+            box-shadow: 0 1px 5px rgb(0 0 0 / 20%);
+            padding: 12px 0px;
+          }
+
+          .option {
+            padding: 4px 16px;
           }
         `}
       </style>
