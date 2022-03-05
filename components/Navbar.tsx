76f484b91e003470027ee16feeedc229e3ecdd85
mansisindhu <mansisindhu2909@gmail.com>
2022-03-05 15:55:15 +0530

    Create navbar for landing page

---
+++ components/Navbar.tsx
@@ -0,0 +1,40 @@
+const Navbar = () => {
+  return (
+    <>
+      <nav className="navbar">
+        <p className="heading">Where in the world?</p>
+        <button className="theme-toggler">Theme Toggle</button>
+      </nav>
+
+      <style jsx>
+        {`
+          .navbar {
+            display: flex;
+            justify-content: space-between;
+            align-items: center;
+            padding: 24px 10px;
+            background-color: #fff;
+            position: fixed;
+            top: 0;
+            right: 0;
+            left: 0;
+            box-shadow: 0 1px 5px rgb(0 0 0 / 20%);
+          }
+
+          .heading {
+            font-size: 14px;
+            font-weight: bold;
+          }
+
+          .theme-toggler {
+            border: none;
+            background-color: #fff;
+            font-size: 13px;
+          }
+        `}
+      </style>
+    </>
+  );
+};
+
+export default Navbar;
