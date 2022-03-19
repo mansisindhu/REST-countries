455134566777e505afd1f75872fd41b251d5f89e
mansisindhu <mansisindhu2909@gmail.com>
2022-03-20 00:21:06 +0530

    Add framer motion

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,16 @@
 import "../styles/global.css";
+import { LayoutGroup } from "framer-motion";
+import { useRouter } from "next/router";
 
 const CustomApp = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+  const router = useRouter();
+  return (
+    <>
+      <LayoutGroup>
+        <Component {...pageProps} key={router.route} />
+      </LayoutGroup>
+    </>
+  );
 };
 
 export default CustomApp;
